e229fe90e7091a7c6209a956d257432412b18c22
Nils <nilsvonnorsinski@gmail.com>
2019-08-26 14:14:50 +0200

    save generated random points to restore them

--- sampling_of_nonlandslides.R
+++ sampling_of_nonlandslides.R
@@ -7,14 +7,14 @@ library(rgeos)
 library(dismo)
 
 # input path
-path_to_images = "Daten/Paldau/Parameters/filtered_images_planCurvature"
+path_to_images = "Daten/Paldau/Parameters/filtered_images_twi"
 path_to_giant = "Daten/Paldau/Landslides/Giant_Paldau.shp"
 path_to_minor = "Daten/Paldau/Landslides/Landslides_Paldau_02_18.shp"
 
 # ouput path
 path_to_out = "Daten/Paldau/Samples"
 name_mask = "maskPaldau"
-name_random_values = "rnd_no_lsd_Paldau_planCurv"
+name_random_values = "rnd_no_lsd_Paldau_twi"
 
 # read images
 giant = st_read(path_to_giant)
@@ -30,7 +30,9 @@ fs = list.files(path=path_to_images, pattern = "tif$", full.names = TRUE)
 fs
 rasStack = stack(fs)
 
+# only run this section if no mask layer is calculatet  ------------------------
 # create mask image
+
 dim = dim(rasStack)
 r = raster(ncol= dim[2], nrow=dim[1])
 extent(r) = extent(rasStack)
@@ -42,16 +44,23 @@ mask[is.na(mask[])] = 2
 # change values where a polygon is to NA
 mask[mask == 1] = NA
 plot(mask)
-# change twos to ones. In fact this is not necessary
+# change twos back to ones. In fact this is not necessary
 mask[mask == 2] = 1
 
-plot(mask)
 
 crs(mask) = crs(rasStack)
 
 # write mask to disk
 writeRaster(mask, filename=file.path(path_to_out, name_mask),
             format = "GTiff", overwrite = TRUE)
+
+# same random points for all samplings
+rnd_poi = as.data.frame(randomPoints(mask = rasStack, n = 400))
+rnd_poi
+
+saveRDS(rnd_poi, "Daten/Paldau/Samples/randomPoints.rds")
+
+# end of region not to run------------------------------------------------------
 
 # create new raster Stack, with mask image as first image
 fs = c(paste0(path_to_out,"/", name_mask,".tif"), fs)
@@ -77,8 +86,8 @@ rasStack = stack(fs)
 
 #start_time = Sys.time()
 
-rnd_poi = as.data.frame(randomPoints(mask = rasStack, n = 10000))
-rnd_poi
+
+
 ext = extract(rasStack, rnd_poi)
 ext
 
@@ -86,7 +95,6 @@ ext
 
 #time = end_time - start_time
 #time
-# 10 000 samples ~ 30 minutes
 
 samples_df = as.data.frame(t(ext))
 
