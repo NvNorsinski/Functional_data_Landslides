984ad0e4e09bace57761eb4cacccf9d385a32f50
NvNorsinski <36541056+NvNorsinski@users.noreply.github.com>
2020-01-17 20:23:57 +0100

    calculate probability map for functional data

--- rastertest.R
+++ rastertest.R
@@ -3,20 +3,14 @@
 rm(list = ls(all = TRUE))
 library(fda)
 library(fda.usc)
-library(ggplot2)
-library(rasterVis)
-library(rgdal)
 
-library(sf)
 library(raster)
-library(foreach)
-library(doParallel)
+
 #-------------------------------------------------------------------------------
 prepath = "C:/Users/Nils-Laptop/Documents/Master"
 path_to_images = "Daten/Paldau/Parameters/"
 
-img = brick(paste0(prepath, "/Filtered_images_tpi/Filtered_images_tpitpi_120.tif"))
-img
+
 
 model = readRDS("Daten/Paldau/Samples/model.rds" )
 #-------------------------------------------------------------------------------
@@ -52,19 +46,23 @@ basis.b = create.bspline.basis(rangeval, norder = polynomial_order, breaks = kno
 
 
 
-chunk_size = 1024
+chunk_size = 150
 
 resolution = c(8517, 10464)
+
 resolution/chunk_size
 
 res = resolution %/% chunk_size
 res
+
 rest = resolution %% chunk_size
+rest
+
 
 chunk_x = c(rep(chunk_size, res[1]), rest[1])
-chunk_x =
+chunk_x
 
-chunk_y = c(rep(chunk_size, res[2]), rest[2])
+
 
 chunk_x_idx = 0
 
@@ -74,65 +72,70 @@ for (i in 1:length(chunk_x)) {
 
 }
 
-chunk_y_idx = 0
-
-# create indices
-for (i in 1:length(chunk_y)) {
-  chunk_y_idx[i] = sum(chunk_y[1:i])
-
-}
-
 
 fs = list.files(path=prepath, pattern = "tif$", full.names = TRUE, recursive = TRUE)
 fs
 
 
-
+chunk_x_idx_upper = chunk_x_idx
+chunk_x_idx_upper
+chunk_x_idx_lower = chunk_x_idx -99
+chunk_x_idx_lower
+chunk_x_idx_lower[1] = chunk_x_idx_lower[1]+1
+chunk_x_idx_lower
 
 rasStack = stack(fs)
+pred_image = 0
+
+for (k in 1:length(chunk_x_idx_upper)) {
+  print(k)
+
+  k = 1
+
+  dat = getValuesBlock(rasStack, chunk_x_idx_lower[k], chunk_x_idx_upper[k], 2, 10464)
+
+
+  dat = as.data.frame(dat)
+
+
+  dat = split.default(dat, rep(1:7, each = 22) )
+  names = c("aspect_ns", "aspect_ow", "catchmant_area", "genCurvature", "slope", "tpi", "twi")
+  names(dat) = names
+
+  data = list()
+
+  j = 1
+
+
+
+  for (i in dat) {
+    i = t(i)
+    is = smooth.basis(tvec, i, basis.x)
+    data[[j]] = fdata(is$fd)
+
+
+    j = j+1
+  }
+  rm(is, i)
+
+
+
+  data2 = list(aspect_ns = data[[1]], aspect_ow = data[[2]], catchmant_area = data[[3]], genCurvature = data[[4]],
+               slope = data[[5]], tpi = data[[6]], twi = data[[7]])
+
+
+
+  pred = predict(model, newx = data2, type = "response", se.fit = TRUE, level = 0.95)
 
 
 
 
-dat = getValuesBlock(rasStack, 2, 1, 2, 10464)
-dat
+  pred = as.data.frame(pred)
 
-dat = as.data.frame(dat)
-
-
-l = split.default(dat,rep(1:7, each = 22) )
-names = c("aspect_ns", "aspect_ow", "catchmant_area", "genCurvature", "slope", "tpi", "twi")
-names(l) = names
-
-data = list()
-
-j = 1
+  pred_image = rbind(pred_image, pred)
+  saveRDS(pred_image, "Daten/Paldau/Parameters/pred_image.rds")
 
 
-
-for (i in l) {
-  i = t(i)
-  is = smooth.basis(tvec, i, basis.x)
-  data[[j]] = fdata(is$fd)
-
-  j = j+1
 }
 
 
-
-data2 = list(aspect_ns = data[[1]], aspect_ow = data[[2]], catchmant_area = data[[3]], genCurvature = data[[4]],
-             slope = data[[5]], tpi = data[[6]], twi = data[[7]])
-
-datadata = data2$aspect_ns
-datadata
-
-
-pred = predict(model, newx = data2, type = "response", se.fit = TRUE, level = 0.95)
-pred
-
-yfit.test = ifelse(pred < 0.5, 0, 1)
-yfit.test
-
-pred = as.data.frame(pred)
-summary(pred)
-ss = as.data.frame(pred)
