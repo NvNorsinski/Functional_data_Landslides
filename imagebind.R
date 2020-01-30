c48d6b1e198eebe7f2fb6c323db6ee679a151147
NvNorsinski <36541056+NvNorsinski@users.noreply.github.com>
2020-01-30 20:17:05 +0100

    upt

--- imagebind.R
+++ imagebind.R
@@ -2,12 +2,22 @@ rm(list = ls(all = TRUE))
 library(raster)
 library(rgdal)
 
-path = "D:/Master/"
+# resolution auf 1 m eter
+
+path = "Daten/Paldau/data_map_fregre/"
+source_img = raster("Daten/Paldau/Outputs/prob_map_glm.tif")
+source_img
+
+crs = crs(source_img)
+ext = extent(source_img)
+res = res(source_img)
+
 files = list.files(path = path, pattern = ".rds")
 files
 
 dimension = c(10464, 350)
 image_comp = 0
+
 
 # create probability map--------------------------------------------------------
 for (k in files) {
@@ -32,9 +42,12 @@ image_comp = rbind(image_comp, img)
 
 dim(image_comp)
 r = raster(image_comp)
-plot(r)
 
-writeRaster(r, "D:/Master/prob_map_fregre.glm",  format="GTiff", overwrite=TRUE)
+extent(r) = ext
+#res(r) = res
+crs(r) = crs
+
+writeRaster(r, "Daten/Paldau/Outputs/prob_map_fregre.glm",  format="GTiff", overwrite=TRUE)
 #create error map---------------------------------------------------------------
 image_comp_err = 0
 for (k in files) {
@@ -58,6 +71,9 @@ image_comp_err = rbind(image_comp_err, img)
 
 dim(image_comp_err)
 r = raster(image_comp_err)
+extent(r) = ext
+crs(r) = crs
 plot(r)
+r
 
-writeRaster(r, "D:/Master/prob_map_fregre.glm_error",  format="GTiff", overwrite=TRUE)
+writeRaster(r, "Daten/Paldau/Outputs/prob_map_fregre.glm_error",  format="GTiff", overwrite=TRUE)
